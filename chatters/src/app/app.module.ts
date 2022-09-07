e0766f38b7a38eae3cfe02268c72aa480e19dbd2
JakeDundas <jakedundas@proton.me>
2022-09-08 09:52:55 +1000

    feat: add component "chat-main"

--- chatters/src/app/app.module.ts
+++ chatters/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatMainComponent } from './chat-main/chat-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMainComponent
   ],
   imports: [
     BrowserModule,
