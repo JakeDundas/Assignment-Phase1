600f718a41e235e3f29e9f56a9a935e0f5f7d3b8
JakeDundas <jakedundas@proton.me>
2022-09-08 09:58:55 +1000

    feat: add chat-main to path ''

--- chatters/src/app/app-routing.module.ts
+++ chatters/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatMainComponent } from './chat-main/chat-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: ChatMainComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
