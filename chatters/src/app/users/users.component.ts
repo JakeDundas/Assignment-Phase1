0dc977811607c51a4f964e2592695095538f42cb
JakeDundas <jakedundas@proton.me>
2022-09-09 09:32:01 +1000

    feat: add users template

--- chatters/src/app/users/users.component.ts
+++ chatters/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  usersList: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.usersList = JSON.parse(localStorage.getItem('usersList') ?? "[]");
   }
+
+  addNewUser(username:string, email:string) {
+    // if(groupName == "") {
+    //   groupName = "Default"
+    // }
+    // const myuuid = uuidv4();
+    // const currentGroups = JSON.parse(localStorage.getItem('groupsList') ?? "[]");
+    // currentGroups.push({name: groupName, id: myuuid});
+    // localStorage.setItem('groupsList', JSON.stringify(currentGroups))
+    this.ngOnInit();
+  }  
+
 
 }
