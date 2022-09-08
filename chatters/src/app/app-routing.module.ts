3a5a36dedf0d4e9ae9df01fd791cff7886861bf0
JakeDundas <jakedundas@proton.me>
2022-09-08 11:52:21 +1000

    feat: add route for "Groups"

--- chatters/src/app/app-routing.module.ts
+++ chatters/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatMainComponent } from './chat-main/chat-main.component';
+import { ChatGroupsComponent } from './chat-groups/chat-groups.component';
 
-const routes: Routes = [{path: '', component: ChatMainComponent}];
+const routes: Routes = [{path: '', component: ChatMainComponent},
+                        {path: 'groups', component: ChatGroupsComponent},
+                      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
