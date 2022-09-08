ab0e8a7bc034a555345f564a5161d8eb6a2641df
JakeDundas <jakedundas@proton.me>
2022-09-08 11:35:03 +1000

    feat: add component "groups"

--- chatters/src/app/app.module.ts
+++ chatters/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatMainComponent } from './chat-main/chat-main.component';
+import { ChatGroupsComponent } from './chat-groups/chat-groups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatMainComponent
+    ChatMainComponent,
+    ChatGroupsComponent
   ],
   imports: [
     BrowserModule,
