510a3ba2e48479834e6ff116fb426a991741eb92
JakeDundas <jakedundas@proton.me>
2022-09-08 12:37:59 +1000

    feat: add dynamic join group buttons
    And dummy data

--- chatters/src/app/chat-groups/chat-groups.component.ts
+++ chatters/src/app/chat-groups/chat-groups.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-groups.component.css']
 })
 export class ChatGroupsComponent implements OnInit {
-
+  groupsList = [{name: "Group1", id: 1}, {name: "Group2", id: 2}, {name: "Group3", id: 3}, ];
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  navigateToChat(groupId: any) {
+
   }
 
 }
