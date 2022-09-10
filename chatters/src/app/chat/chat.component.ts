918e601a909f438064eb621342cb3aeda220401f
JakeDundas <jakedundas@proton.me>
2022-09-10 16:06:45 +1000

    feat: add Add/Remove user to chat component

--- chatters/src/app/chat/chat.component.ts
+++ chatters/src/app/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Channel } from '../shared/channel.model';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-chat',
@@ -13,6 +14,7 @@ export class ChatComponent implements OnInit {
   group: any;
   currentChannel: Channel | null = null;
   messageContent: string = "";
+  username: string = "";
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,7 +22,7 @@ export class ChatComponent implements OnInit {
     this.groupId = this.route.snapshot.params['groupId'];
     this.group = this.getCurrentGroupFromStorage();
   }
-  
+
   getCurrentGroupFromStorage() {
     const groups = JSON.parse(localStorage.getItem('groupsList') ?? "[]");
     const index = groups.findIndex((group: { id: string; }) => group.id == this.groupId);
@@ -31,8 +33,8 @@ export class ChatComponent implements OnInit {
     if (this.currentChannel) {
       this.currentChannel.active = false;
     }
+    channel.active = true;
     this.currentChannel = channel;
-    this.currentChannel.active = true;
   }
 
   addNewChannel() {
@@ -40,11 +42,55 @@ export class ChatComponent implements OnInit {
     this.updateStorage();
     this.ngOnInit();
   }
-  
+
   deleteChannel(channelId: string) {
+    // TODO: deal with fringe case of deleting active channel
     const index = this.group.channels.findIndex((channel: { id: string; }) => channel.id == channelId);
     if (index > -1) {
       this.group.channels.splice(index, 1)
+      this.updateStorage();
+      this.ngOnInit();
+    }
+  }
+
+  addUserToGroup() {
+    const currentUsers = JSON.parse(localStorage.getItem('usersList') ?? "[]");
+    const userExists = currentUsers.some((user: { username: string; }) => user.username == this.username);
+    const userInGroup = this.group.users.includes(this.username);
+    if (userExists && !userInGroup) {
+      this.group.users.push(this.username);
+      this.updateStorage();
+      this.ngOnInit();
+    }
+  }
+
+  addUserToSelectedChannel() {
+    if (this.currentChannel) {
+      const userInGroup = this.group.users.includes(this.username);
+      const userInChannel = this.currentChannel.users.includes(this.username);
+      if (userInGroup && !userInChannel) {
+        this.currentChannel.users.push(this.username);
+        this.updateStorage();
+        this.ngOnInit();
+      }
+    }
+  }
+
+  removeUser(user: string, channelId: string) {
+    const index = this.group.channels.findIndex((channel: { id: string; }) => channel.id == channelId);
+    if (index > -1) {
+      let channelUsers = this.group.channels[index].users;
+      channelUsers.splice(channelUsers.indexOf(user, 1));
+      this.updateStorage();
+      this.ngOnInit();
+    }
+  }
+
+  promoteToGroupAssis(user: string) {
+    const userInGroup = this.group.users.includes(this.username);
+    const userInGroupAssis = this.group.groupAssisUsers.includes(user);
+    if (userInGroup && !userInGroupAssis) {
+      this.group.groupAssisUsers.push(user);
       this.updateStorage();
       this.ngOnInit();
     }
@@ -58,7 +104,7 @@ export class ChatComponent implements OnInit {
   }
 
   addNewMessage() {
-    if(this.currentChannel) {
+    if (this.currentChannel) {
       this.currentChannel.messageHistory.push(this.messageContent);
       this.updateStorage();
       this.messageContent = "";
