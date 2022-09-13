4e0e844fce6e596955cf175da8b064e28ff1cc06
JakeDundas <jakedundas@proton.me>
2022-09-13 20:32:12 +1000

    feat: add logOut() to navbar

--- chatters/src/app/app.component.ts
+++ chatters/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatters';
+
+  constructor(private router: Router) {}
+
+  logOut() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("role");
+    this.router.navigate(['login'])
+  }
 }
