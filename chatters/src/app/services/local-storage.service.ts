eb17c2787d91ed9f63ca8bbed6fdb52297966d6b
JakeDundas <jakedundas@proton.me>
2022-09-13 20:35:55 +1000

    feat: add replacer(), reviver() to convert JSON to Maps

--- chatters/src/app/services/local-storage.service.ts
+++ chatters/src/app/services/local-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Group } from '../shared/group.model';
+import { User } from '../shared/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,40 @@ export class LocalStorageService {
   constructor() { }
 
   getGroupsData() {
-    const groupsData = JSON.parse(localStorage.getItem('groupsList') ?? "[]");
+    const groupsData = JSON.parse(localStorage.getItem('groupsList') ?? "[]", this.reviver);
     return groupsData;
   }
 
-  setGroupsData(groupsData: { [key: string]: Group }) {
-    localStorage.setItem('groupsList', JSON.stringify(groupsData));
+  setGroupsData(groupsData: Map<string, Group>) {
+    localStorage.setItem('groupsList', JSON.stringify(groupsData, this.replacer));
+  }
+
+  getUsersData() {
+    const usersData = JSON.parse(localStorage.getItem('usersList') ?? "[]", this.reviver);
+    return usersData;
+  }
+
+  setUsersData(usersData: Map<string, User>) {
+    localStorage.setItem('usersList', JSON.stringify(usersData, this.replacer));
+  }
+
+  replacer(key: any, value: any[]) {
+    if(value instanceof Map) {
+      return {
+        dataType: 'Map',
+        value: Array.from(value.entries()), // or with spread: value: [...value]
+      };
+    } else {
+      return value;
+    }
+  }
+
+  reviver(key: any, value: any) {
+    if(typeof value === 'object' && value !== null) {
+      if (value.dataType === 'Map') {
+        return new Map(value.value);
+      }
+    }
+    return value;
   }
 }
