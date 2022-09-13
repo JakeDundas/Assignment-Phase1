9734b0c32226bdec198708f3834da9eea3402ee5
JakeDundas <jakedundas@proton.me>
2022-09-13 20:34:06 +1000

    feat: add login() to login component

--- chatters/src/app/login/login.component.ts
+++ chatters/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string = "";
+  email: string = "";
 
-  constructor() { }
+  constructor(private router: Router, private usersService: UsersService) { }
 
   ngOnInit(): void {
+  }
+
+  attemptLogin() {
+    const validUser = this.usersService.checkForUsernameEmailMatch(this.username, this.email);
+    if (validUser) {
+      localStorage.setItem('role', validUser.role)
+      localStorage.setItem('username', validUser.username)
+      localStorage.setItem('userId', validUser.id)
+      this.router.navigate(['groups'])
+    }
   }
 
   user() {
     localStorage.setItem('role', 'user')
     localStorage.setItem('username', 'jake')
+    localStorage.setItem('userId', "186374b2-44d7-4303-be4c-44589fea7bb7")
   }
   
   groupAdmin() {
