69c8ae8e61fcae7d4bc2f50e0736a13dc8da73aa
JakeDundas <jakedundas@proton.me>
2022-09-13 15:19:51 +1000

    refactor: modify models to use Map instead of Array

--- chatters/src/app/shared/channel.model.ts
+++ chatters/src/app/shared/channel.model.ts
@@ -2,13 +2,26 @@ import { v4 as uuidv4 } from 'uuid';
 import { User } from './user.model';
 
 export class Channel {
+    name: string;
     id: string;
-    public users: string[] = [];
-    public messageHistory: string[] = [];
-    public active: boolean = false;
+    users = new Map<string, User>;
+    messageHistory: string[] = [];
+    active: boolean = false;
 
-    constructor(public name: string) {
-        this.name = name;
-        this.id = uuidv4();
+    constructor(public channelObject: any) {
+        if(channelObject.id) {
+            this.name = channelObject.name;
+            this.id = channelObject.id;
+            this.messageHistory = channelObject.messageHistory;
+            this.active = channelObject.active;
+
+            channelObject.users.forEach((userObject: any) => {
+                const user = new User(userObject);
+                this.users.set(user.id, user);
+            });
+        } else {
+            this.name = channelObject.name;
+            this.id = uuidv4();
+        }
     }
 }
