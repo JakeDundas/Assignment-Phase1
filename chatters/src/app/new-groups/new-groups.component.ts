201c37af59f2bc8d00e18e68bf804eb171efa717
JakeDundas <jakedundas@proton.me>
2022-10-18 19:36:43 +1000

    refactor: restye chat-groups

---
+++ chatters/src/app/new-groups/new-groups.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Group } from '../shared/group.model';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-new-groups',
+  templateUrl: './new-groups.component.html',
+  styleUrls: ['./new-groups.component.css']
+})
+export class NewGroupsComponent implements OnInit {
+  groupName: string = "";
+  groupsList: Group[] = [];
+  isGroupAdminOrSuperAdmin: boolean = true;
+
+  constructor(private router: Router, private dataService: DataService) {
+    if (localStorage.getItem('isLoggedIn') != 'true') {
+      this.router.navigate(['login'])
+    }
+  }
+
+  ngOnInit(): void {
+    this.initialiseGroupsForUser();
+  }
+
+  initialiseGroupsForUser() {
+    const currentRole = localStorage.getItem('role');
+    if (currentRole == 'groupAdmin' || currentRole == 'superAdmin') {
+      this.isGroupAdminOrSuperAdmin = true;
+      this.getAllGroups();
+    } else {
+      this.isGroupAdminOrSuperAdmin = false;
+      this.getGroupsForUser();
+    }
+  }
+
+  getAllGroups() {
+    this.dataService.getAllGroups().subscribe((res: any) => {
+      this.groupsList = res.groups;
+    })
+  }
+
+  getGroupsForUser() {
+    this.dataService.getGroupsForUser({ _id: localStorage.getItem('userId') }).subscribe((res: any) => {
+      this.groupsList = res.groups;
+    })
+  }
+
+  navigateToChat(groupId: any) {
+    this.router.navigate(['chat', groupId])
+  }
+
+  addNewGroup() {
+    if (this.groupName != "") {
+      this.dataService.addNewGroup({ name: this.groupName }).subscribe((res: any) => {
+        this.ngOnInit();
+      })
+      this.groupName = "";
+    }
+  }
+
+}
